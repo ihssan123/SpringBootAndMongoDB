0d3aa27235df9492a5f0e85db498c0de1ddb97fe
ihssan123 <121307161+ihssan123@users.noreply.github.com>
2023-10-17 18:47:21 +0100

    first commit

---
+++ src/main/java/com/example/SpringBootAndMongoDB/SpringBootAndMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAndMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndMongoDbApplication.class, args);
+	}
+
+}
